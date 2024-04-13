d62b81a5287f9455a7a47708474c376c157b6548
admin <qikai@playcrab.com>
2024-04-13 14:48:41 +0800

    增加引入impl

--- cmdb/apps/init.go
+++ cmdb/apps/init.go
@@ -3,5 +3,5 @@ package apps
 import (
 	_ "github.com/GeekQk/devcloud-mini/cmdb/apps/resource/impl"
 	_ "github.com/GeekQk/devcloud-mini/cmdb/apps/secret/api"
-	// _ "github.com/GeekQk/devcloud-mini/cmdb/apps/secret/impl"
+	_ "github.com/GeekQk/devcloud-mini/cmdb/apps/secret/impl"
 )
