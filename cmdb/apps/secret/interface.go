dbea8b80fe274d04a5ed9cd3cdf50c808044fa26
admin <qikai@playcrab.com>
2024-04-13 08:34:28 +0800

    cmdb 设计

--- cmdb/apps/secret/interface.go
+++ cmdb/apps/secret/interface.go
@@ -1,5 +1,48 @@
 package secret
 
+import (
+	"context"
+
+	resource "github.com/GeekQk/devcloud-mini/cmdb/apps/resource"
+)
+
 const (
 	AppName = "secret"
 )
+
+type Service interface {
+	// 录入云商凭证
+	CreateSecret(context.Context, *CreateSecretRequest) (*Secret, error)
+	// 查询云商凭证
+	DescribeSecret(context.Context, *DescribeSecretRequest) (*Secret, error)
+
+	// 使用云商凭证同步资源 Stream
+	// SyncResourceHandler 回调参数, 把同步完成的资源, 通过cb交给外部处理
+	// 使用 channel   resource chan[SyncResponse]， channel 是锁结构, 回调是无锁结构
+	SyncResource(context.Context, *SyncResourceRequest, SyncResourceHandler) error
+}
+
+// 通过Hook返回 资源是否同步成功
+type SyncResourceHandler func(*SyncResponse)
+
+type SyncResponse struct {
+	Id    string
+	Name  string
+	Error string
+}
+
+type SyncResourceRequest struct {
+	// secret id
+	Id string
+	// 同步那些区域的资源
+	Region []string
+	// 同步那些类型的资源
+	Resource []resource.TYPE
+}
+
+func NewCreateSecretRequest() *CreateSecretRequest {
+	return &CreateSecretRequest{
+		Regions:       []string{},
+		ResourceTypes: []resource.TYPE{},
+	}
+}
