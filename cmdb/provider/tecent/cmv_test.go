a2c21a23f80a4cecdae1d7e2c00c8cd60a769b6b
admin <qikai@playcrab.com>
2024-04-14 10:55:23 +0800

    cvm test

--- cmdb/provider/tecent/cmv_test.go
+++ cmdb/provider/tecent/cmv_test.go
@@ -2,9 +2,12 @@ package tecent_test
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/GeekQk/devcloud-mini/cmdb/apps/resource"
+	"github.com/GeekQk/devcloud-mini/cmdb/provider"
+	"github.com/GeekQk/devcloud-mini/cmdb/provider/tecent"
 )
 
 var ctx = context.Background()
@@ -17,13 +20,13 @@ var ctx = context.Background()
 // 	},
 // }
 
-// var client = &tecent.CvmProvider{
-// 	provider.ResourceSyncConfig{
-// 		ApiSecret: os.Getenv("API_SECRET"),
-// 		ApiKey:    os.Getenv("API_KEY"),
-// 		Region:    "ap-guangzhou",
-// 	},
-// }
+var client = &tecent.CvmProvider{
+	provider.ResourceSyncConfig{
+		ApiSecret: os.Getenv("API_SECRET"),
+		ApiKey:    os.Getenv("API_KEY"),
+		Region:    "ap-guangzhou",
+	},
+}
 
 func TestSync(t *testing.T) {
 	err := client.Sync(ctx, func(ctx context.Context, r *resource.Resource) {
